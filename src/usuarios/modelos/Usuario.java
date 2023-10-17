621e8f4d67521d8afc122a1f0805af8b9f74e2a9
Norberto Rodriguez Vaquero <norberto.rodriguezvaquero@gmail.com>
2023-10-17 16:12:06 -0300

    Punto 3

---
+++ src/usuarios/modelos/Usuario.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package usuarios.modelos;
+
+/**
+ *
+ * @author estudiante
+ */
+public abstract class Usuario {
+    private String correo;
+    private String contrasenia;
+    private String nombre;
+    private String apellido;
+    
+    public void mostrar() {
+        System.out.println("Nombre: " + nombre
+                + "\nApellido: " + apellido
+                + "\nCorreo: " + correo
+                + "\nContraseña: " + contrasenia);
+    }
+    
+    public String obtenerCorreo() {
+        return correo;
+    }
+
+    public void asignarCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String obtenerContrasenia() {
+        return contrasenia;
+    }
+
+    public void asignarContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    public void asignarNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String obtenerApellido() {
+        return apellido;
+    }
+
+    public void asignarApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+}

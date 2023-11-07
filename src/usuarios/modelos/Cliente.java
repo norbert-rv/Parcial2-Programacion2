a31504a4290c5d9c70ef746fd67637af66acf192
Norberto Rodriguez Vaquero <norberto.rodriguezvaquero@gmail.com>
2023-11-06 23:06:08 -0300

    Agregada comprobación de que el pedido no sea null en agregarPedido(Pedido) y cancelarPedido(Pedido) según sugerencia en la correción del TP4

--- src/usuarios/modelos/Cliente.java
+++ src/usuarios/modelos/Cliente.java
@@ -25,25 +25,34 @@ public class Cliente extends Usuario {
     }
 
     @Override
-    public ArrayList<Pedido> verPedidos(){
+    public ArrayList<Pedido> verPedidos() {
         return pedidos;
     }
-    
+
     public void agregarPedido(Pedido pedido) {
         /*
         Si el pedido no está en el ArrayList, lo agrega al final. Si
         el pedido está en el ArrayList, reemplaza el pedido con el 
         método set(index, element)... reemplaza por element el elemento
         que está en la posición index.
-        */
-        if(!pedidos.contains(pedido))
+         */
+
+        if (pedido == null) {
+            return;
+        }
+
+        if (!pedidos.contains(pedido)) {
             pedidos.add(pedido);
-        else
+        } else {
             pedidos.set(pedidos.indexOf(pedido), pedido);
+        }
+
     }
-    
+
     public void cancelarPedido(Pedido pedido) {
-        pedidos.remove(pedido);
+        if (pedido != null) {
+            pedidos.remove(pedido);
+        }
     }
-    
+
 }

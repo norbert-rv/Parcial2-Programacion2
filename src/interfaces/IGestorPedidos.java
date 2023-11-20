80651f8b571a2041c9404e9a737415593b529f9d
Norberto Rodriguez Vaquero <norberto.rodriguezvaquero@gmail.com>
2023-11-20 13:08:14 -0300

    Corregidos errores al copiar y pegar los metodos abstractos en las clases

--- src/interfaces/IGestorPedidos.java
+++ src/interfaces/IGestorPedidos.java
@@ -17,24 +17,31 @@ import usuarios.modelos.Cliente;
  * @author estudiante
  */
 public interface IGestorPedidos {
-    public String crearPedido(LocalDate fecha, LocalTime hora,
-ArrayList<ProductoDelPedido> productosDelPedido, Cliente cliente);
-public String cambiarEstado(Pedido pedidoAModificar);
-public ArrayList<Pedido> verPedidos();
-public boolean hayPedidosConEsteCliente(Cliente cliente);
-public boolean hayPedidosConEsteProducto(Producto producto);
-public String cancelarPedido(Pedido pedido);
-public boolean existeEstePedido(Pedido pedido);
-public Pedido obtenerPedido(Integer numero);
 
-public static final String EXITO = "Pedido creado/modificado/cancelado con éxito";
-public static final String ERROR_FECHA = "La fecha del pedido es incorrecta";
-public static final String ERROR_HORA = "La hora del pedido es incorrecta";
-public static final String ERROR_PRODUCTOS_DEL_PEDIDO = "El pedido no tiene productos";
-public static final String ERROR_CLIENTE = "El pedido no tiene un cliente";
-public static final String ERROR_ESTADO = "El pedido no tiene un estado";
-public static final String ERROR_CANCELAR = "No se puede cancelar el pedido en este estado";
-public static final String PEDIDOS_DUPLICADOS = "Ya existe un pedido con ese número";
-public static final String PEDIDO_INEXISTENTE = "No existe el pedido especificado";
-public static final String VALIDACION_EXITO = "El pedido tiene los datos correctos";
+    public String crearPedido(LocalDate fecha, LocalTime hora, ArrayList<ProductoDelPedido> productosDelPedido, Cliente cliente);
+
+    public String cambiarEstado(Pedido pedidoAModificar);
+
+    public ArrayList<Pedido> verPedidos();
+
+    public boolean hayPedidosConEsteCliente(Cliente cliente);
+
+    public boolean hayPedidosConEsteProducto(Producto producto);
+
+    public String cancelarPedido(Pedido pedido);
+
+    public boolean existeEstePedido(Pedido pedido);
+
+    public Pedido obtenerPedido(Integer numero);
+
+    public static final String EXITO = "Pedido creado/modificado/cancelado con éxito";
+    public static final String ERROR_FECHA = "La fecha del pedido es incorrecta";
+    public static final String ERROR_HORA = "La hora del pedido es incorrecta";
+    public static final String ERROR_PRODUCTOS_DEL_PEDIDO = "El pedido no tiene productos";
+    public static final String ERROR_CLIENTE = "El pedido no tiene un cliente";
+    public static final String ERROR_ESTADO = "El pedido no tiene un estado";
+    public static final String ERROR_CANCELAR = "No se puede cancelar el pedido en este estado";
+    public static final String PEDIDOS_DUPLICADOS = "Ya existe un pedido con ese número";
+    public static final String PEDIDO_INEXISTENTE = "No existe el pedido especificado";
+    public static final String VALIDACION_EXITO = "El pedido tiene los datos correctos";
 }
--- src/interfaces/IGestorUsuarios.java
+++ src/interfaces/IGestorUsuarios.java
@@ -13,23 +13,28 @@ import usuarios.modelos.Usuario;
  * @author estudiante
  */
 public interface IGestorUsuarios {
-    public String crearUsuario(String correo, String apellido, String nombre, Perfil
-perfil, String clave, String claveRepetida);
-public ArrayList<Usuario> verUsuarios();
-public ArrayList<Usuario> buscarUsuarios(String apellido);
-public String borrarUsuario(Usuario usuario);
-public boolean existeEsteUsuario(Usuario usuario);
-public Usuario obtenerUsuario(String correo);
 
-public static final String EXITO = "Usuario creado/modificado con éxito";
-public static final String ERROR_CORREO = "El correo del usuario es incorrecto";
-public static final String ERROR_APELLIDO = "El apellido del usuario es incorrecto";
-public static final String ERROR_NOMBRE = "El nombre del usuario es incorrecto";
-public static final String ERROR_CLAVES = "Las claves especificadas no coinciden o son incorrectas"; 
-public static final String ERROR_PERFIL = "El perfil del usuario es incorrecto";
-public static final String PERFIL_CAMBIO = "El usuario no puede cambiarse de perfil";
-public static final String ERROR_PERMISOS = "No se tienen los permisos para realizar esta funcionalidad";
-public static final String USUARIOS_DUPLICADOS = "Ya existe un usuario con ese correo";
-public static final String USUARIO_INEXISTENTE = "No existe el usuario especificado";
-public static final String VALIDACION_EXITO = "Los datos del usuario son correctos";
+    public String crearUsuario(String correo, String apellido, String nombre, Perfil perfil, String clave, String claveRepetida);
+
+    public ArrayList<Usuario> verUsuarios();
+
+    public ArrayList<Usuario> buscarUsuarios(String apellido);
+
+    public String borrarUsuario(Usuario usuario);
+
+    public boolean existeEsteUsuario(Usuario usuario);
+
+    public Usuario obtenerUsuario(String correo);
+
+    public static final String EXITO = "Usuario creado/modificado con éxito";
+    public static final String ERROR_CORREO = "El correo del usuario es incorrecto";
+    public static final String ERROR_APELLIDO = "El apellido del usuario es incorrecto";
+    public static final String ERROR_NOMBRE = "El nombre del usuario es incorrecto";
+    public static final String ERROR_CLAVES = "Las claves especificadas no coinciden o son incorrectas";
+    public static final String ERROR_PERFIL = "El perfil del usuario es incorrecto";
+    public static final String PERFIL_CAMBIO = "El usuario no puede cambiarse de perfil";
+    public static final String ERROR_PERMISOS = "No se tienen los permisos para realizar esta funcionalidad";
+    public static final String USUARIOS_DUPLICADOS = "Ya existe un usuario con ese correo";
+    public static final String USUARIO_INEXISTENTE = "No existe el usuario especificado";
+    public static final String VALIDACION_EXITO = "Los datos del usuario son correctos";
 }

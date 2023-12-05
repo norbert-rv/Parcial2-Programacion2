e23b8c0eb2019ea93f5b88b3374d0e5d9433d658
TomasCabreraBellomo <tomycabrera2003@gmail.com>
2023-12-05 17:37:07 -0300

    agregadas las interfaces que nos dieron en la consigna, listas para implementar

---
+++ src/principal/vistas/IControladorPrincipal.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package principal.vistas;
+import java.awt.event.ActionEvent;
+/**
+ *
+ * @author tomascabrerabellomo
+ */
+public interface IControladorPrincipal {
+    public static final String TITULO = "Bar";
+    
+    /**
+     * Acción a ejecutar cuando se selecciona el botón Usuarios
+     * @param evt evento
+     */                        
+    public void btnUsuariosClic(ActionEvent evt); 
+    
+    /**
+     * Acción a ejecutar cuando se selecciona el botón Productos
+     * @param evt evento
+     */                        
+    public void btnProductosClic(ActionEvent evt); 
+    
+    /**
+     * Acción a ejecutar cuando se selecciona el botón Pedidos
+     * @param evt evento
+     */                        
+    public void btnPedidosClic(ActionEvent evt); 
+    
+    /**
+     * Acción a ejecutar cuando se selecciona el botón Cancelar
+     * @param evt evento
+     */                        
+    public void btnSalirClic(ActionEvent evt); 
+}
---
+++ src/usuarios/vistas/IControladorAMUsuario.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package usuarios.vistas;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+/**
+ *
+ * @author tomascabrerabellomo
+ */
+public interface IControladorAMUsuario {
+    public static final String TITULO_NUEVO = "Nuevo usuario"; 
+    public static final String TITULO_MODIFICAR = "Modificar usuario";
+    
+    /**
+     * Acción a ejecutar cuando se selecciona el botón Guardar
+     * @param evt evento
+     */                        
+    public void btnGuardarClic(ActionEvent evt);
+
+    /**
+     * Acción a ejecutar cuando se selecciona el botón Cancelar
+     * @param evt evento
+     */                        
+    public void btnCancelarClic(ActionEvent evt);
+    
+    /**
+     * Acción a ejecutar cuando se presiona una tecla en el campo txtApellido
+     * @param evt evento
+     */
+    public void txtApellidoPresionarTecla(KeyEvent evt);
+    
+    /**
+     * Acción a ejecutar cuando se presiona una tecla en el campo txtNombre
+     * @param evt evento
+     */
+    public void txtNombrePresionarTecla(KeyEvent evt);
+    
+    /**
+     * Acción a ejecutar cuando se presiona una tecla en el campo txtDocumento
+     * @param evt evento
+     */
+    public void txtCorreoPresionarTecla(KeyEvent evt);        
+        
+    
+    /**
+     * Acción a ejecutar cuando se presiona una tecla en el campo passClave
+     * @param evt evento
+     */
+    public void passClavePresionarTecla(KeyEvent evt); 
+    
+    /**
+     * Acción a ejecutar cuando se presiona una tecla en el campo passRepetirClave
+     * @param evt evento
+     */
+    public void passClaveRepetidaPresionarTecla(KeyEvent evt);
+}

efe851e8d741502df8505ce969e3d413a9c76ba2
Norberto Rodriguez Vaquero <norberto.rodriguezvaquero@gmail.com>
2023-12-07 14:14:36 -0300

    Modificación de VentanaPrincipal para que el controlador principal maneje los eventos

--- src/principal/vistas/VentanaPrincipal.java
+++ src/principal/vistas/VentanaPrincipal.java
@@ -4,17 +4,23 @@
  */
 package principal.vistas;
 
+import interfaces.IControladorPrincipal;
+
 /**
  *
  * @author tomascabrerabellomo
  */
 public class VentanaPrincipal extends javax.swing.JFrame {
 
+    // creación de la referencia para el controlador principal
+    private IControladorPrincipal controlador;
+    
     /**
      * Creates new form VentanaPrincipal
      */
-    public VentanaPrincipal() {
+    public VentanaPrincipal(IControladorPrincipal controlador) {
         initComponents();
+        this.controlador=controlador; // en el constructor de la ventana defino el controlador
     }
 
     /**
@@ -48,8 +54,18 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         });
 
         botonpedidos.setText("Pedidos");
+        botonpedidos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonpedidosActionPerformed(evt);
+            }
+        });
 
         botonsalir.setText("Salir");
+        botonsalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonsalirActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -84,47 +100,55 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void botonusuariosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonusuariosActionPerformed
-        // TODO add your handling code here:
+        this.controlador.btnUsuariosClic(evt);
     }//GEN-LAST:event_botonusuariosActionPerformed
 
     private void botonproductosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonproductosActionPerformed
-        // TODO add your handling code here:
+        this.controlador.btnProductosClic(evt);
     }//GEN-LAST:event_botonproductosActionPerformed
+
+    private void botonsalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonsalirActionPerformed
+        this.controlador.btnSalirClic(evt);
+    }//GEN-LAST:event_botonsalirActionPerformed
+
+    private void botonpedidosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonpedidosActionPerformed
+        this.controlador.btnPedidosClic(evt);
+    }//GEN-LAST:event_botonpedidosActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new VentanaPrincipal().setVisible(true);
-            }
-        });
-    }
+//    public static void main(String args[]) {
+//        /* Set the Nimbus look and feel */
+//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+//         */
+//        try {
+//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//                if ("Nimbus".equals(info.getName())) {
+//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        } catch (ClassNotFoundException ex) {
+//            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (InstantiationException ex) {
+//            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (IllegalAccessException ex) {
+//            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//            java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        }
+//        //</editor-fold>
+//
+//        /* Create and display the form */
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                new VentanaPrincipal().setVisible(true);
+//            }
+//        });
+//    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton botonpedidos;

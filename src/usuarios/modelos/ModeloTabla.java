0458a92c6a0ade9c881c4e05e17d9c424365be20
Norberto Rodriguez Vaquero <norberto.rodriguezvaquero@gmail.com>
2023-12-12 14:02:33 -0300

    Get/set para usuarios.

--- src/usuarios/modelos/ModeloTabla.java
+++ src/usuarios/modelos/ModeloTabla.java
@@ -16,74 +16,82 @@ import javax.swing.table.AbstractTableModel;
 public class ModeloTabla extends AbstractTableModel {
 
     // IMPLEMENTAR
-    
     private List<Usuario> usuarios = new ArrayList<>();
     private List<String> nombresColumnas = new ArrayList<>();
-    
+
     public ModeloTabla() {
         this.nombresColumnas.add("Apellido");
         this.nombresColumnas.add("Nombre");
         this.nombresColumnas.add("Perfil");
-        
+
         IGestorUsuarios gu = GestorUsuarios.instanciar();
-        
+
         this.usuarios = gu.verUsuarios();
     }
-    
+
     @Override
     public int getRowCount() {
-       return this.usuarios.size();
+        return this.usuarios.size();
     }
-    
+
     @Override
     public int getColumnCount() {
         return this.nombresColumnas.size();
     }
-    
+
     @Override
     public Object getValueAt(int fila, int columna) {
-        
+
         Usuario usuario = usuarios.get(fila);
-        
-        switch(columna) {
-            case 0: 
+
+        switch (columna) {
+            case 0:
                 return usuario.verApellido();
-            case 1: 
+            case 1:
                 return usuario.verNombre();
-            case 2: 
-                if(usuario instanceof Cliente)
+            case 2:
+                if (usuario instanceof Cliente) {
                     return Perfil.CLIENTE.toString();
-                else if(usuario instanceof Empleado)
+                } else if (usuario instanceof Empleado) {
                     return Perfil.EMPLEADO.toString();
-                else
+                } else {
                     return Perfil.ENCARGADO.toString();
+                }
         }
-        
+
         return "Error";
     }
-    
+
     @Override
     public String getColumnName(int columna) {
         return this.nombresColumnas.get(columna);
     }
-    
+
     public void actualizarDatosTabla() {
         IGestorUsuarios gu = GestorUsuarios.instanciar();
-        
+
         this.usuarios = gu.verUsuarios();
-        
+
         // método de TableModel para notificar al JTable que debe actualizar los datos
         this.fireTableDataChanged();
     }
-    
+
     // sobrecarga del método actualizarDatosTabla()
     public void actualizarDatosTabla(List<Usuario> usuarios) {
         this.usuarios = usuarios;
         this.fireTableDataChanged();
     }
-    
-    // VER CUALES HAY QUE IMPLEMENTAR
-    
+
+    public List<Usuario> obtenerUsuarios() {
+        return usuarios;
+    }
+
+    public void asignarUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+}
+
+// VER CUALES HAY QUE IMPLEMENTAR
 //    @Override
 //    public boolean isCellEditable(int fila, int columna) {
 //        return true;
@@ -98,4 +106,3 @@ public class ModeloTabla extends AbstractTableModel {
 //    public Class<> getColumnClass(int columna) {
 //        
 //    }
-}
